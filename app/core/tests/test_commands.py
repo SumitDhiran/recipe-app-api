2173d5180014a9429677ee68ca71583cd184905c
SumitDhiran <sumitdhiran5@gmail.com>
2023-03-07 23:51:01 +0530

    fixed some linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,7 +10,8 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test Commands."""
 
-    def test_wait_for_db_ready(self,patched_check):
+
+    def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database is ready."""
         patched_check.return_value = True
 
@@ -19,10 +20,10 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self,patched_sleep,patched_check):
+    def test_wait_for_db_delay(self,patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 +[True]
+            [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
